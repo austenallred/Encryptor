e575f48052349687f6672aa13fb63b3fb0d7c7b6
Austen Allred <austen.allred@gmail.com>
2014-01-07 21:43:02 -0700

    finished encryption

--- encryptor.rb
+++ encryptor.rb
@@ -1,4 +1,21 @@
 class Encryptor
+  def encrypt_letter(letter)
+    lowercase_letter = letter.downcase
+    cipher[lowercase_letter]
+  end
+
+  def encrypt(string)
+  letters = string.split("")
+
+  results = []
+  letters.each do |letter|
+    encrypted_letter = encrypt_letter(letter)
+    results.push(encrypted_letter)
+  end
+
+  results.join
+end
+# Join the results in one string
   def cipher
     {'a' => 'а', 'b' => 'б', 'c' => 'ц', 'd' => 'д', 'e' => 'е', 'f' => 'ф', 'g' => 'г', 
    'h' => 'ч', 'i' => 'и', 'j' => 'й', 'k' => 'к', 'l' => 'л', 'm' => 'м', 'n' => 'н', 
@@ -6,4 +23,3 @@ class Encryptor
    'v' => 'в', 'w' => 'ш', 'x' => 'х', 'y' => 'ы', 'z' => 'з',  }
   end
 end
-
