def4ad9622ea4e60c577c91c7e37416961bcaeac
Austen Allred <austen.allred@gmail.com>
2014-01-08 14:36:15 -0700

    encryptor working and using arrays to strings

--- encryptor.rb
+++ encryptor.rb
@@ -6,13 +6,13 @@ class Encryptor
 
   def encrypt(string)
     letters = string.split("")
-    results = []
-    letters.collect do |letter|
-      encrypted_letter = encrypt_letter(letter)
-      results.push(encrypted_letter)
+    results = letters.collect do |letter|
+      encrypt_letter(letter)
   end
-end
-# Join the results in one string
+      end_results = results.join
+      puts "#{end_results}"
+  end
+
   def cipher
     {'a' => 'а', 'b' => 'б', 'c' => 'ц', 'd' => 'д', 'e' => 'е', 'f' => 'ф', 'g' => 'г', 
    'h' => 'ч', 'i' => 'и', 'j' => 'й', 'k' => 'к', 'l' => 'л', 'm' => 'м', 'n' => 'н', 
@@ -20,10 +20,4 @@ end
    'v' => 'в', 'w' => 'ш', 'x' => 'х', 'y' => 'ы', 'z' => 'з', 'nil' =>' '}
   end
 
-  def decipher
-    {'а' => 'a', 'б' => 'b', 'ц' => 'c', 'д' => 'd', 'е' => 'e', 'ф' => 'f', 'г' => 'g', 
-   'ч' => 'h', 'и' => 'i', 'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 
-   'о' => 'о', 'п' => 'p', 'я' => 'q', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 
-   'в' => 'v', 'ш' => 'w', 'х' => 'x', 'ы' => 'y', 'з' => 'z',  }
-  end
 end
