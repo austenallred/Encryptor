4342f234123c93bc33921844b75dca84a98af617
Austen Allred <austen.allred@gmail.com>
2014-01-07 19:32:02 -0700

    starting over

--- encryptor.rb
+++ encryptor.rb
@@ -1,15 +1,4 @@
 class Encryptor
-    def encrypt_letter(letter)
-      lowercase_letter = letter.downcase
-      cipher[lowercase_letter]
-    end
-
-    def encrypt(string)
-      # Cut the string into letters
-      letters = string.split("")
-      # Encrypt the letters one at a time
-      # Join the results back together in one string
-    end
   def cipher
     {'a' => 'а', 'b' => 'б', 'c' => 'ц', 'd' => 'д', 'e' => 'е', 'f' => 'ф', 'g' => 'г', 
    'h' => 'ч', 'i' => 'и', 'j' => 'й', 'k' => 'к', 'l' => 'л', 'm' => 'м', 'n' => 'н', 
@@ -17,3 +6,4 @@ class Encryptor
    'v' => 'в', 'w' => 'ш', 'x' => 'х', 'y' => 'ы', 'z' => 'з',  }
   end
 end
+
