7b5c11434e449ab1686830cfd213f236b01680fb
Austen Allred <austen.allred@gmail.com>
2014-01-08 08:10:51 -0700

    added collect method

--- encryptor.rb
+++ encryptor.rb
@@ -5,21 +5,25 @@ class Encryptor
   end
 
   def encrypt(string)
-  letters = string.split("")
-
-  results = []
-  letters.each do |letter|
-    encrypted_letter = encrypt_letter(letter)
-    results.push(encrypted_letter)
+    letters = string.split("")
+    
+    letters.collect do |letter|
+      encrypted_letter = encrypt_letter(letter)
+      
   end
-
-  results.join
 end
 # Join the results in one string
   def cipher
     {'a' => 'а', 'b' => 'б', 'c' => 'ц', 'd' => 'д', 'e' => 'е', 'f' => 'ф', 'g' => 'г', 
    'h' => 'ч', 'i' => 'и', 'j' => 'й', 'k' => 'к', 'l' => 'л', 'm' => 'м', 'n' => 'н', 
    'o' => 'о', 'p' => 'п', 'q' => 'я', 'r' => 'р', 's' => 'с', 't' => 'т', 'u' => 'у', 
-   'v' => 'в', 'w' => 'ш', 'x' => 'х', 'y' => 'ы', 'z' => 'з',  }
+   'v' => 'в', 'w' => 'ш', 'x' => 'х', 'y' => 'ы', 'z' => 'з', 'nil' =>' '}
+  end
+
+  def decipher
+    {'а' => 'a', 'б' => 'b', 'ц' => 'c', 'д' => 'd', 'е' => 'e', 'ф' => 'f', 'г' => 'g', 
+   'ч' => 'h', 'и' => 'i', 'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 
+   'о' => 'о', 'п' => 'p', 'я' => 'q', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 
+   'в' => 'v', 'ш' => 'w', 'х' => 'x', 'ы' => 'y', 'з' => 'z',  }
   end
 end
